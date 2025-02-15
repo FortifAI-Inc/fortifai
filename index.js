ecd784643eda3f0f5de5e1f1dadf65a857d9ade0
Hilik Stein <hilik.stein@gmail.com>
2025-02-15 16:49:14 +0000

    bugfix

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const detector = require('./src/detector');
+const flow_logs = require('./src/flow_logs');
 
 console.log("Starting Fortifai...");
 
 async function main() {
     try {
         await detector.inventoryAWSEnvironment();
-        await detector.collectFlowLogs();
+        await flow_logs.collectFlowLogs();
     } catch (error) {
         console.error("Error in main function:", error);
     }
