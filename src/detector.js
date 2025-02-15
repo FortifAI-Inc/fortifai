e6c6511c2cb2611afa47790b358c4cbfa4bcf16b
Hilik Stein <hilik.stein@gmail.com>
2025-02-15 17:54:04 +0200

    adde more verbose printing

--- src/detector.js
+++ src/detector.js
@@ -19,14 +19,25 @@ async function inventoryAWSEnvironment() {
         console.log("EC2 Instances:", instanceList);
         console.log("EC2 Instances count:", instanceList.length);
 
+        // Get VPCs
+        const vpcs = await ec2.describeVpcs().promise();
+        console.log("VPCs:", vpcs.Vpcs);
+        console.log("VPCs count:", vpcs.Vpcs.length);
+
         // Get S3 buckets
         const s3Buckets = await s3.listBuckets().promise();
+        console.log("S3 Buckets:", s3Buckets.Buckets);
+        console.log("S3 Buckets count:", s3Buckets.Buckets.length);
 
         // Get RDS instances
         const rdsInstances = await rds.describeDBInstances().promise();
+        console.log("RDS Instances:", rdsInstances.DBInstances);
+        console.log("RDS Instances count:", rdsInstances.DBInstances.length);
 
         // Get Lambda functions
         const lambdaFunctions = await lambda.listFunctions().promise();
+        console.log("Lambda Functions:", lambdaFunctions.Functions);
+        console.log("Lambda Functions count:", lambdaFunctions.Functions.length);
 
         // Get VPCs
         //const vpcs = await vpc.describeVpcs().promise();
