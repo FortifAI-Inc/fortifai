a870e4c1e403cbf8ee5a836722a8d99e8387b526
Hilik Stein <hilik.stein@gmail.com>
2025-02-15 15:22:00 +0200

    Implemented inventory detection

--- src/detector.js
+++ src/detector.js
@@ -1,8 +1,50 @@
 const AWS = require('aws-sdk');
 
-function inventoryAWSEnvironment() {
+AWS.config.update({ region: 'us-east-1' }); // Update to your region
+
+// This function should interface with AWS's infrastructure and obtain a list of all provisioned elements - EC2, VPCs, SGs etc
+// This function should be called at the start of the program
+async function inventoryAWSEnvironment() {
     console.log("Inventorying AWS environment...");
-    // TODO: Implement AWS environment inventory logic
+    const ec2 = new AWS.EC2();
+    const s3 = new AWS.S3();
+    const rds = new AWS.RDS();
+    const lambda = new AWS.Lambda();
+    const vpc = new AWS.VPC();
+
+    try {
+        // Get EC2 instances
+        const ec2Instances = await ec2.describeInstances().promise();
+        const instanceList = ec2Instances.Reservations.map(reservation => reservation.Instances).flat();
+
+        // Get S3 buckets
+        const s3Buckets = await s3.listBuckets().promise();
+
+        // Get RDS instances
+        const rdsInstances = await rds.describeDBInstances().promise();
+
+        // Get Lambda functions
+        const lambdaFunctions = await lambda.listFunctions().promise();
+
+        // Get VPCs
+        const vpcs = await vpc.describeVpcs().promise();
+
+
+        // Compile the inventory
+        const inventory = {
+            ec2Instances: instanceList,
+            s3Buckets: s3Buckets.Buckets,
+            rdsInstances: rdsInstances.DBInstances,
+            lambdaFunctions: lambdaFunctions.Functions,
+            vpcs: vpcs.Vpcs
+        };
+
+        return inventory;
+    } catch (error) {
+        console.error("Error retrieving AWS resources:", error);
+        throw error;
+    }
+
 }
 
 function collectRunningProcesses() {
