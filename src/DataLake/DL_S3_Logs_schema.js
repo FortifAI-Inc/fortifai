ed087119fc1a844716c6a36594ba3644fe7d11aa
Hilik Stein <hilik.stein@gmail.com>
2025-03-13 20:46:19 +0200

    implementing S3 logging schema and event logger

---
+++ src/DataLake/DL_S3_Logs_schema.js
@@ -0,0 +1,53 @@
+const commonSchema = new parquet.ParquetSchema({
+    eventID: { type: 'UTF8' },
+    eventTime: { type: 'TIMESTAMP_MILLIS' },
+    eventSource: { type: 'UTF8' },
+    eventName: { type: 'UTF8' },
+    awsRegion: { type: 'UTF8' },
+    sourceIPAddress: { type: 'UTF8' },
+    userAgent: { type: 'UTF8' },
+    readOnly: { type: 'BOOLEAN' },
+    eventType: { type: 'UTF8' },
+    managementEvent: { type: 'BOOLEAN' },
+    recipientAccountId: { type: 'UTF8' },
+    eventCategory: { type: 'UTF8' },
+    requestID: { type: 'UTF8' },
+    userType: { type: 'UTF8' },               // IAMUser, AssumedRole, AWSService
+    principalId: { type: 'UTF8' },            // AWS principal ID
+    userArn: { type: 'UTF8' },                // ARN of the user making the request
+    userName: { type: 'UTF8' },               // IAM username (if applicable)
+    accessKeyId: { type: 'UTF8' },            // Access Key used for authentication
+    accountId: { type: 'UTF8' },              // AWS Account ID of the user
+    // **Session Context (If AssumedRole or Session-Based Events)**
+    //TODO: understand this part better
+    //sessionIssuerArn: { type: 'UTF8', optional: true },  // ARN of the session issuer
+    //sessionIssuerType: { type: 'UTF8', optional: true }, // Type of session issuer (Role, User, etc.)
+    //sessionIssuerPrincipalId: { type: 'UTF8', optional: true }, // Principal ID of session issuer
+    //sessionCreationDate: { type: 'TIMESTAMP_MILLIS', optional: true }, // Session creation timestamp
+    //mfaAuthenticated: { type: 'BOOLEAN', optional: true }, // Whether MFA was used
+});
+
+
+const eventSchemas = {
+    RunInstances: new parquet.ParquetSchema({
+        eventID: { type: 'UTF8' },
+        instanceType: { type: 'UTF8' },
+        imageId: { type: 'UTF8' },
+        privateIpAddress: { type: 'UTF8' },
+        securityGroup: { type: 'UTF8' },
+    }),
+    AssumeRole: new parquet.ParquetSchema({
+        eventID: { type: 'UTF8' },
+        roleArn: { type: 'UTF8' },
+        roleSessionName: { type: 'UTF8' },
+        credentials: { type: 'UTF8' },
+    }),
+    TerminateInstances: new parquet.ParquetSchema({
+        eventID: { type: 'UTF8' },
+        instanceId: { type: 'UTF8' }
+    }),
+    CreateBucket: new parquet.ParquetSchema({
+        eventID: { type: 'UTF8' },
+        bucketName: { type: 'UTF8' }
+    }),
+};
--- src/Utils/EventLogger.js
+++ src/Utils/EventLogger.js
@@ -2,6 +2,33 @@ async function logEvent(eventName, event){
     switch (eventName) {
         case "TerminateInstances":
             console.log("Terminate Instance event: ", event);
+            const EventCommonData = {
+                eventID: event.eventID,
+                eventTime: event.eventTime,
+                eventSource: event.eventSource,
+                eventName: event.eventName, 
+                awsRegion: event.CloudTrailEvent.awsRegion,
+                sourceIPAddress: event.CloudTrailEvent.sourceIPAddress, 
+                userAgent: event.CloudTrailEvent.userAgent,
+                readOnly: event.readOnly,
+                eventType: event.CloudTrailEvent.eventType,
+                managementEvent: event.CloudTrailEvent.managementEvent,
+                recipientAccountId: event.CloudTrailEvent.recipientAccountId,
+                eventCategory: event.CloudTrailEvent.eventCategory,
+                requestID: event.CloudTrailEvent.requestID,
+                userType: event.userIdentity.type,
+                principalId: event.userIdentity.principalId,
+                userArn: event.userIdentity.arn,
+                userName: event.userIdentity.userName,
+                accessKeyId: event.AccessKeyId,
+                accountId: event.userIdentity.accountId
+            }
+            const EventPrivateData = {
+                eventID: event.eventID,
+                instanceId: event.CloudTrailEvent.requestParameters.instancesSet.items[0].instanceId,
+            }
+            console.log("EventCommonData: ", EventCommonData);
+            console.log("EventPrivateData: ", EventPrivateData);
             break;
         default:
             console.log("Unknown event: ", event);
