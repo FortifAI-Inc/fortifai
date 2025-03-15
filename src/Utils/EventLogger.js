2fce4e0c52240c2184314a338b3e70d0020f41b1
Hilik Stein <hilik.stein@gmail.com>
2025-03-15 19:48:42 +0200

    fixes

--- src/Utils/EventLogger.js
+++ src/Utils/EventLogger.js
@@ -81,27 +81,6 @@ async function logEvent(eventName, event) {
     switch (eventName) {
         case "TerminateInstances":
             //console.log("Terminate Instance event: ", event);
-            const EventCommonData = {
-                EventId: event.EventId,
-                EventTime: Number(event.EventTime),
-                EventSource: event.EventSource,
-                EventName: event.EventName,
-                awsRegion: CloudTrailEvent.awsRegion,
-                sourceIPAddress: CloudTrailEvent.sourceIPAddress,
-                userAgent: CloudTrailEvent.userAgent,
-                ReadOnly: event.ReadOnly,
-                eventType: CloudTrailEvent.eventType,
-                managementEvent: CloudTrailEvent.managementEvent,
-                recipientAccountId: CloudTrailEvent.recipientAccountId,
-                eventCategory: CloudTrailEvent.eventCategory,
-                requestID: CloudTrailEvent.requestID,
-                userType: CloudTrailEvent.userIdentity.type,
-                principalId: CloudTrailEvent.userIdentity.principalId,
-                userArn: CloudTrailEvent.userIdentity.arn,
-                userName: CloudTrailEvent.userIdentity.userName,
-                accessKeyId: event.AccessKeyId,
-                accountId: CloudTrailEvent.userIdentity.accountId
-            }
             EventPrivateData = {
                 EventId: event.EventId,
                 instanceId: CloudTrailEvent.requestParameters.instancesSet.items[0].instanceId,
